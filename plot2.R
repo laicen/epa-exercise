a1c728a9173d74f4da4862931579400e771fcf81
laicen <laicenteno@gmail.com>
2015-04-22 23:50:50 +0100

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,8 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+png(file="plot2.png")
+options(scipen=10)
+with(subset(NEI, fips=="24510"), barplot(tapply(Emissions,year,sum), col="red", ylab=expression(paste("Amount of ", PM[2.5], 
+  " emitted, in tons")), xlab="Year", main=expression(paste("Total ",PM[2.5],
+  " Emission from All Sources in Baltimore City, Maryland")), ylim=c(0,3500)))
+dev.off()
