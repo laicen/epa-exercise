7d35af78fc9dd81722829a0df924ea9d163d4215
laicen <laicenteno@gmail.com>
2015-04-22 23:38:45 +0100

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,7 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+png(file="plot1.png")
+options(scipen=10)
+with(NEI, barplot(tapply(Emissions,year,sum), col="red", ylab=expression(paste("Amount of ", PM[2.5], " emitted, in tons")),
+  xlab="Year", main=expression(paste("Total ",PM[2.5]," Emission from All Sources")), ylim=c(0,8000000)))
+dev.off()
