e3d9f1336e92a4b177af844879ffbfd6cabdb991
laicen <laicenteno@gmail.com>
2015-04-23 21:54:55 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,9 +1,11 @@
-library(ggplot2)
+library(ggplot2) #Using ggplot2 & plyr packages
 library(plyr)
-NEI <- readRDS("summarySCC_PM25.rds")
+NEI <- readRDS("summarySCC_PM25.rds") #Reading RDS data into R
 SCC <- readRDS("Source_Classification_Code.rds")
-png(file="plot3.png")
-type.NEI<-ddply(subset(NEI,fips=="24510"), .(year,type), summarize, Emissions=sum(Emissions))
+png(file="plot3.png") #Saving plot into image data
+type.NEI<-ddply(subset(NEI,fips=="24510"), .(year,type), summarize, Emissions=sum(Emissions)) #Subsetting data and getting totals
+type.NEI$year <- as.character(type.NEI$year) #Changing class of year variable to character
+#Plotting using qplot(ggplot2 basic), using bar graph, annotating graph, setting limits
 qplot(x=year,y=Emissions, data=type.NEI, geom="bar", stat="identity", facets=.~type, 
   ylab=expression(paste("Amount of ", PM[2.5], " emitted, in tons")), 
   xlab="Year", main=expression(paste(PM[2.5]," Emission by Type of Source in Baltimore City, Maryland")),
