a4dfe085d1771d1505ba2ea11db02af88d7bdc82
laicen <laicenteno@gmail.com>
2015-04-23 02:05:36 +0100

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+library(plyr)
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+png(file="plot3.png")
+type.NEI<-ddply(subset(NEI,fips=="24510"), .(year,type), summarize, Emissions=sum(Emissions))
+qplot(x=year,y=Emissions, data=type.NEI, geom="bar", stat="identity", facets=.~type, 
+  ylab=expression(paste("Amount of ", PM[2.5], " emitted, in tons")), 
+  xlab="Year", main=expression(paste("Total ",PM[2.5]," Emission by Type of Source in Baltimore City, Maryland")),
+  ylim=c(0,2500))
+dev.off()
