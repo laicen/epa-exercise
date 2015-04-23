180142b38e7e5cab6e72c087f678f12670d1291c
laicen <laicenteno@gmail.com>
2015-04-23 19:48:23 +0100

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -7,9 +7,7 @@ unique(SCC$EI.Sector)
 coal.SCC <- SCC[SCC$EI.Sector %in% c("Fuel Comb - Electric Generation - Coal",
   "Fuel Comb - Industrial Boilers, ICEs - Coal", "Fuel Comb - Comm/Institutional - Coal"), ]
 coal <- NEI[NEI$SCC %in% coal.SCC$SCC , ]
-barplot(
-qplot(x=year,y=Emissions, data=type.NEI, geom="bar", stat="identity", facets=.~type, 
-  ylab=expression(paste("Amount of ", PM[2.5], " emitted, in tons")), 
-  xlab="Year", main=expression(paste("Total ",PM[2.5]," Emission by Type of Source in Baltimore City, Maryland")),
-  ylim=c(0,2500))
+with(coal, barplot(tapply(Emissions,year,sum), col="red", ylab=expression(paste("Amount of ", PM[2.5], " emitted, in tons")),
+  xlab="Year", main=expression(paste(PM[2.5]," Emission from Coal Combustion Related Sources in the US")),
+  ylim=c(0,600000)))
 dev.off()
