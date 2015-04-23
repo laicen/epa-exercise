88f7cc7f55d89a02542351904e5a20c0cc3e5a47
laicen <laicenteno@gmail.com>
2015-04-23 22:20:33 +0100

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -1,14 +1,16 @@
-library(ggplot2)
+library(ggplot2) #Using ggplot2 & plyr packages
 library(plyr)
-NEI <- readRDS("summarySCC_PM25.rds")
+NEI <- readRDS("summarySCC_PM25.rds") #Reading RDS data in R
 SCC <- readRDS("Source_Classification_Code.rds")
-png(file="plot6.png")
-motor.SCC <- SCC[SCC$Data.Category == "Onroad" , ]
-motor.NEI <- NEI[NEI$SCC %in% motor.SCC$SCC , ]
+png(file="plot6.png") #Saving image
+motor.SCC <- SCC[SCC$Data.Category == "Onroad" , ] #Subsetting SCC data with Onroad Data Category
+motor.NEI <- NEI[NEI$SCC %in% motor.SCC$SCC , ] #Subsetting NEI data with Onroad Data Category
+#Subsetting NEI data to get emissions coming from Baltimore City & LA County, getting totals
 compare.NEI<-ddply(subset(NEI,fips == "24510" | fips == "06037"), .(year,fips), summarize, Emissions=sum(Emissions))
 compare.NEI$year <- as.character(compare.NEI$year) #Changing year variable to character class
 compare.NEI #Printing the data to determine values to be used for limits
 compare.NEI$fips <- c("06037" = "Los Angeles County, California", "24510" = "Baltimore City, Maryland") #Renaming fips
+#Using qplot to graph bar chart, annotating graph, setting limit
 qplot(x=year,y=Emissions, data=compare.NEI, geom="bar", stat="identity", facets=.~fips, 
   ylab=expression(paste("Amount of ", PM[2.5], " emitted, in tons")), 
   xlab="Year", main=expression(paste(PM[2.5]," Emission from Motor Vehicle Sources")), ylim=c(0,50000))
