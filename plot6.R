7790a1f3113938bc9068c64239487c3c216a7061
laicen <laicenteno@gmail.com>
2015-04-23 23:03:17 +0100

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -6,12 +6,12 @@ png(file="plot6.png") #Saving image
 motor.SCC <- SCC[SCC$Data.Category == "Onroad" , ] #Subsetting SCC data with Onroad Data Category
 motor.NEI <- NEI[NEI$SCC %in% motor.SCC$SCC , ] #Subsetting NEI data with Onroad Data Category
 #Subsetting NEI data to get emissions coming from Baltimore City & LA County, getting totals
-compare.NEI<-ddply(subset(NEI,fips == "24510" | fips == "06037"), .(year,fips), summarize, Emissions=sum(Emissions))
+compare.NEI<-ddply(subset(motor.NEI,fips == "24510" | fips == "06037"), .(year,fips), summarize, Emissions=sum(Emissions))
 compare.NEI$year <- as.character(compare.NEI$year) #Changing year variable to character class
 compare.NEI #Printing the data to determine values to be used for limits
 compare.NEI$fips <- c("06037" = "Los Angeles County, California", "24510" = "Baltimore City, Maryland") #Renaming fips
 #Using qplot to graph bar chart, annotating graph, setting limit
 qplot(x=year,y=Emissions, data=compare.NEI, geom="bar", stat="identity", facets=.~fips, 
   ylab=expression(paste("Amount of ", PM[2.5], " emitted, in tons")), 
-  xlab="Year", main=expression(paste(PM[2.5]," Emission from Motor Vehicle Sources")), ylim=c(0,50000))
+  xlab="Year", main=expression(paste(PM[2.5]," Emission from Motor Vehicle Sources")), ylim=c(0,5000))
 dev.off()
