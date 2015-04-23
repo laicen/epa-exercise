261291f0c525374d0c2f5043e553557486979133
laicen <laicenteno@gmail.com>
2015-04-23 21:47:27 +0100

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -6,9 +6,10 @@ png(file="plot6.png")
 motor.SCC <- SCC[SCC$Data.Category == "Onroad" , ]
 motor.NEI <- NEI[NEI$SCC %in% motor.SCC$SCC , ]
 compare.NEI<-ddply(subset(NEI,fips == "24510" | fips == "06037"), .(year,fips), summarize, Emissions=sum(Emissions))
+compare.NEI$year <- as.character(compare.NEI$year) #Changing year variable to character class
+compare.NEI #Printing the data to determine values to be used for limits
+compare.NEI$fips <- c("06037" = "Los Angeles County, California", "24510" = "Baltimore City, Maryland") #Renaming fips
 qplot(x=year,y=Emissions, data=compare.NEI, geom="bar", stat="identity", facets=.~fips, 
   ylab=expression(paste("Amount of ", PM[2.5], " emitted, in tons")), 
-  xlab="Year", main=expression(paste(PM[2.5]," Emission from Motor Vehicle Sources in Baltimore City, Maryland 
-  & Los Angeles, California")),
-  ylim=c(0,2500))
+  xlab="Year", main=expression(paste(PM[2.5]," Emission from Motor Vehicle Sources")), ylim=c(0,50000))
 dev.off()
