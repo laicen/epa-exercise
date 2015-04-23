bb4603e6a00bf1860e991af5e0b19de24bf615d6
laicen <laicenteno@gmail.com>
2015-04-23 20:46:12 +0100

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -3,9 +3,12 @@ library(plyr)
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 png(file="plot6.png")
-type.NEI<-ddply(subset(NEI,fips=="24510"), .(year,type), summarize, Emissions=sum(Emissions))
-qplot(x=year,y=Emissions, data=type.NEI, geom="bar", stat="identity", facets=.~type, 
+motor.SCC <- SCC[SCC$Data.Category == "Onroad" , ]
+motor.NEI <- NEI[NEI$SCC %in% motor.SCC$SCC , ]
+compare.NEI<-ddply(subset(NEI,fips == "24510" | fips == "06037"), .(year,fips), summarize, Emissions=sum(Emissions))
+qplot(x=year,y=Emissions, data=compare.NEI, geom="bar", stat="identity", facets=.~fips, 
   ylab=expression(paste("Amount of ", PM[2.5], " emitted, in tons")), 
-  xlab="Year", main=expression(paste(PM[2.5]," Emission by Type of Source in Baltimore City, Maryland")),
+  xlab="Year", main=expression(paste(PM[2.5]," Emission from Motor Vehicle Sources in Baltimore City, Maryland 
+  & Los Angeles, California")),
   ylim=c(0,2500))
 dev.off()
