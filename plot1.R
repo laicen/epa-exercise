7720d5c737f20a15832eac00a18800c42bdf6c12
laicen <laicenteno@gmail.com>
2015-04-23 22:03:53 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,8 @@
-NEI <- readRDS("summarySCC_PM25.rds")
+NEI <- readRDS("summarySCC_PM25.rds") #Reading RDS data in R
 SCC <- readRDS("Source_Classification_Code.rds")
-png(file="plot1.png")
-options(scipen=10)
+png(file="plot1.png") #Saving plot into image data
+options(scipen=10) #Increasing allocation for number digits
+#Basic plotting using barplot, annotating graph, setting limits
 with(NEI, barplot(tapply(Emissions,year,sum), col="red", ylab=expression(paste("Amount of ", PM[2.5], " emitted, in tons")),
   xlab="Year", main=expression(paste(PM[2.5]," Emission from All Sources in the US")), ylim=c(0,8000000)))
 dev.off()
