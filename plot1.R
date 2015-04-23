e3007147c76f78278d9696692aa8902334ba0e38
laicen <laicenteno@gmail.com>
2015-04-23 19:49:16 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,5 +3,5 @@ SCC <- readRDS("Source_Classification_Code.rds")
 png(file="plot1.png")
 options(scipen=10)
 with(NEI, barplot(tapply(Emissions,year,sum), col="red", ylab=expression(paste("Amount of ", PM[2.5], " emitted, in tons")),
-  xlab="Year", main=expression(paste("Total ",PM[2.5]," Emission from All Sources in US")), ylim=c(0,8000000)))
+  xlab="Year", main=expression(paste(PM[2.5]," Emission from All Sources in the US")), ylim=c(0,8000000)))
 dev.off()
